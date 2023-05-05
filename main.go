f105e103ff0431945d3f3686f59f2b255b2de4cf
webdestroya <webdestroya@users.noreply.github.com>
2023-05-04 18:32:40 -0700

    updates

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package awseventgenerator
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"io"
 	"net/url"
 	"os"
 	"path"
+	"strings"
 )
 
 func GenerateFromSchemaFile(filename string, config *Config) ([]byte, error) {
@@ -61,6 +64,58 @@ func GenerateFromSchema(schema *Schema, config *Config) ([]byte, error) {
 	}
 
 	return buf.Bytes(), nil
+}
+
+// data =
+// string = file (unless it starts with {)
+// []byte = interpreted as raw schema
+// *Schema
+// io.Reader
+func Generate(from any, config *Config) ([]byte, error) {
+	switch val := from.(type) {
+	case []byte:
+		return GenerateFromSchemaString(string(val), config)
+
+	case string:
+		// if it smells like json, just assume it is
+		if strings.ContainsAny(val, `{},"[]`) {
+			return GenerateFromSchemaString(val, config)
+		}
+
+		return GenerateFromSchemaFile(val, config)
+	case io.Reader:
+		filedata, err := io.ReadAll(val)
+		if err != nil {
+			return nil, err
+		}
+		return GenerateFromSchemaString(string(filedata), config)
+	case *Schema:
+		return GenerateFromSchema(val, config)
+	default:
+		return nil, fmt.Errorf("invalid source type: %T", from)
+	}
+}
+
+func GenerateAndExport(data any, destinationFile string, config *Config) error {
+
+	output, err := Generate(data, config)
+	if err != nil {
+		return err
+	}
+
+	destDir := path.Dir(destinationFile)
+
+	if _, err := os.Stat(destDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
+			return fmt.Errorf("Could not make directories for: %s %w", destDir, err)
+		}
+	}
+
+	if err := os.WriteFile(destinationFile, output, 0o600); err != nil {
+		return fmt.Errorf("Could not write file: %s %w", destinationFile, err)
+	}
+
+	return nil
 }
 
 func abs(name string) (string, error) {
--- main_test.go
+++ main_test.go
@@ -2,10 +2,13 @@ package awseventgenerator_test
 
 import (
 	"os"
+	"path"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/webdestroya/awseventgenerator"
+	"github.com/webdestroya/awseventgenerator/internal/testutil/genhelpers"
 )
 
 const (
@@ -58,5 +61,92 @@ func TestGenerateFromSchema(t *testing.T) {
 		expectedOut, err := awseventgenerator.GenerateFromSchemaFile(testSchemaFile, &genEntrypointsTestConfig)
 		require.NoError(t, err)
 		require.Equal(t, string(expectedOut), string(out))
+	})
+}
+
+func TestGenerate(t *testing.T) {
+
+	fileData := genhelpers.MustRet(os.ReadFile(testSchemaFile))
+	schema := genhelpers.MustRet(awseventgenerator.Parse(string(fileData), nil))
+
+	expectedOut := genhelpers.MustRet(awseventgenerator.GenerateFromSchema(schema, &genEntrypointsTestConfig))
+
+	tables := []struct {
+		label    string
+		from     any
+		errMatch any
+	}{
+		{
+			label: "bytearray",
+			from:  fileData,
+		},
+		{
+			label: "filepath",
+			from:  testSchemaFile,
+		},
+		{
+			label: "stringdata",
+			from:  string(fileData),
+		},
+		{
+			label: "schema",
+			from:  schema,
+		},
+		{
+			label: "filehandle",
+			from:  genhelpers.MustRet(os.Open(testSchemaFile)),
+		},
+		{
+			label: "StringReader",
+			from:  strings.NewReader(string(fileData)),
+		},
+
+		// Failures
+		{
+			label:    "MissingFile",
+			from:     "/tmp/fakefile",
+			errMatch: "no such file or directory",
+		},
+
+		{
+			label:    "InvalidFrom",
+			from:     'x',
+			errMatch: "invalid source type",
+		},
+	}
+
+	for _, table := range tables {
+		t.Run(table.label, func(t *testing.T) {
+			out, err := awseventgenerator.Generate(table.from, &genEntrypointsTestConfig)
+			if table.errMatch != nil {
+				require.Error(t, err)
+
+				switch e := table.errMatch.(type) {
+				case string:
+					require.ErrorContains(t, err, e)
+				default:
+					require.FailNowf(t, "unknown error matching type???", "got: %T", table.errMatch)
+				}
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Len(t, out, len(expectedOut))
+			require.Equal(t, expectedOut, out)
+		})
+	}
+
+}
+
+func TestGenerateAndExport(t *testing.T) {
+	t.Run("happy", func(t *testing.T) {
+		dirname := t.TempDir()
+		destFile := path.Join(dirname, "subdir", "gen_export.txt")
+		err := awseventgenerator.GenerateAndExport(testSchemaFile, destFile, &genEntrypointsTestConfig)
+		require.NoError(t, err)
+
+		require.FileExists(t, destFile)
+
 	})
 }
